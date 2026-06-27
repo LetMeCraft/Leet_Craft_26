c5e8402cb6dd1c1ed4b3aee17bf2be0f9971ef4d
Arpit Sharma <meetarpit.codes@gmail.com>
2026-06-27 15:09:57 +0530

    Time: 0 ms (100%), Space: 12.2 MB (72.48%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     void merge(vector<int>& A, int m, vector<int>& B, int n) {
-
-        // Remove the extra 0s from nums1
-        while (A.size() > m) {
-            A.pop_back();
+        int idx = m+n-1, i=m-1, j=n-1;
+        while(i>=0 && j>= 0){
+            if(A[i]>=B[j]){
+                A[idx--] = A[i--];
+            }
+            else{
+                A[idx--] = B[j--];
+            }
         }
-
-        // Append nums2 into nums1
-        A.insert(A.end(), B.begin(), B.end());
-
-        // Sort the merged array
-        sort(A.begin(), A.end());
+        while(j>=0){
+            A[idx--] = B[j--];
+        }
     }
 };
 
